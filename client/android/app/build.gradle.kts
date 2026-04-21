654a95bd2c669b492aa4417e4af8861f17f705dc
Leonardo Lobato <leo@lobato.org>
2026-04-21 11:34:02 +0200

    Bump version to v1.2.2

--- client/android/app/build.gradle.kts
+++ client/android/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "cc.waxid.android"
         minSdk = 29
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.2.1"
+        versionCode = 5
+        versionName = "1.2.2"
     }
 
     val releaseStoreFile = System.getenv("RELEASE_STORE_FILE")
