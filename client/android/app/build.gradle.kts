65fdcae54508f3b6e6fb5f0bf86af7d07a24fa70
Leonardo Lobato <leo@lobato.org>
2026-06-22 14:47:24 +0200

    Bump version to v1.3.0

--- client/android/app/build.gradle.kts
+++ client/android/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "cc.waxid.android"
         minSdk = 29
         targetSdk = 35
-        versionCode = 6
-        versionName = "1.2.3"
+        versionCode = 7
+        versionName = "1.3.0"
     }
 
     val releaseStoreFile = System.getenv("RELEASE_STORE_FILE")
